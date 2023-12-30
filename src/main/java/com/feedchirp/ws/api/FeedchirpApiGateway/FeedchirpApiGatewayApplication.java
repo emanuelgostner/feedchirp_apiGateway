ecc611ed6f42b84cefdd8cf070aeaec52b059229
emanuel <gostner.emanuel@gmail.com>
2023-12-30 21:59:57 +0100

    chapter12.3

---
+++ src/main/java/com/feedchirp/ws/api/FeedchirpApiGateway/FeedchirpApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.feedchirp.ws.api.FeedchirpApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedchirpApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedchirpApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feedchirp/ws/api/FeedchirpApiGateway/FeedchirpApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedchirp.ws.api.FeedchirpApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedchirpApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
